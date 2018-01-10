47e0315d8547cbe876fdb37b1a77d5aef94b0de8
Francisco Javier Martinez Lago <edoreld@gmail.com>
2018-01-10 15:42:03 +0100

    intended script file

--- e-applications/hw0/fibonacciFunctions.js
+++ e-applications/hw0/fibonacciFunctions.js
@@ -1,51 +1,51 @@
 function getFibonacciNumberAtIndexRecursive () {
-			var index = prompt("Please enter the Fibonacci index you want", "");
-			document.getElementById("result").innerHTML = "Result: " + getFibonnaciRecursiveOptimized(index);
-		}
+	var index = prompt("Please enter the Fibonacci index you want", "");
+	document.getElementById("result").innerHTML = "Result: " + getFibonnaciRecursiveOptimized(index);
+}
 
-		function getFibonnaci() {
-			var index = prompt("Please enter the Fibonacci index you want", "");
-			document.getElementById("result").innerHTML = "Result: " + getFibonacciNumberAtIndex(index);
-		}
+function getFibonnaci() {
+	var index = prompt("Please enter the Fibonacci index you want", "");
+	document.getElementById("result").innerHTML = "Result: " + getFibonacciNumberAtIndex(index);
+}
 
-		// 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55 , 89 , 144
-		function getFibonacciNumberAtIndex (index) {
-			if (index == 1 || index == 2) {
-				return 1;
-			}
 
-			prevNum1 = 1, prevNum2 = 1;
-			result = 0;
+function getFibonacciNumberAtIndex (index) {
+	if (index == 1 || index == 2) {
+		return 1;
+	}
 
-			for (i = 2 ; i < index ; i++) { 
-				result = prevNum1 + prevNum2; 
-				prevNum1 = prevNum2; 
-				prevNum2 = result; 
-			}	
+	prevNum1 = 1, prevNum2 = 1;
+	result = 0;
 
-			document.getElementById("result").innerHTML = "Result: " + result;
-			return result;
-		}
+	for (i = 2 ; i < index ; i++) { 
+		result = prevNum1 + prevNum2; 
+		prevNum1 = prevNum2; 
+		prevNum2 = result; 
+	}	
 
-		// Unoptimized recursive function
-		function getFibonnaciRecursive (index) {
-			if (index == 1 || index == 2) {
-				return 1;
-			}
+	document.getElementById("result").innerHTML = "Result: " + result;
+	return result;
+}
 
-			return getFibonnaciRecursive(index - 1) + getFibonnaciRecursive(index - 2);
-		}
+// Unoptimized recursive function
+function getFibonnaciRecursive (index) {
+	if (index == 1 || index == 2) {
+		return 1;
+	}
 
-		// optimized recursive function
-		function getFibonnaciRecursiveOptimized(n) {
-			var a = 1;
-			var b = 0;
-			var c = null;
-			while (n > 0) {
-				c = a;		
-				a = b + a;   
-				b = c;   	
-				n--;
-			}
-			return b;
-		}
+	return getFibonnaciRecursive(index - 1) + getFibonnaciRecursive(index - 2);
+}
+
+// optimized recursive function
+function getFibonnaciRecursiveOptimized(n) {
+	var a = 1;
+	var b = 0;
+	var c = null;
+	while (n > 0) {
+		c = a;		
+		a = b + a;   
+		b = c;   	
+		n--;
+	}
+	return b;
+}
